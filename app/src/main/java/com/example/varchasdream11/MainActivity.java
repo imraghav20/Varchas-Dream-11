a7bbd053262ce801b06697a2b0ad7c079f00ac34
Gaurav Sen <sen.2@iitj.ac.in>
2021-02-17 21:11:22 +0530

    Add SignInActivity backend

--- app/src/main/java/com/example/varchasdream11/MainActivity.java
+++ app/src/main/java/com/example/varchasdream11/MainActivity.java
@@ -14,7 +14,7 @@ import com.example.varchasdream11.databinding.ActivityMainBinding;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    //comment
     ActivityMainBinding binding;
     FirebaseAuth auth;
     @Override
